32748a6fbde8cacc41b811d715460c5641a5988c
Bruno Vilela Batista <brunovilela2002@hotmail.com>
2026-04-09 23:22:55 -0300

    Steps 1 and 2 from Django tutorial

---
+++ kinote/kinoteApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KinoteappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kinoteApp'
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from kinote!")
-
-
-if __name__ == "__main__":
-    main()
